0178593ff0a83dd25222d271a1f179e1be36fdcc
Artem <artemlask2014@gmail.com>
2022-10-30 11:41:37 +0200

    add new script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-alert ("Hello from add-js-script!")
+
+var message = "Hello";
+console.log(message);
+
+message = "World";
+console.log(message);
+
+const mail = "Letter";
+console.log(mail);
+
+let age = 25;
+console.log(age);
+
+age = 28;
+console.log(age);
