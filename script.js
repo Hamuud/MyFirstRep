1c8bc20bf3a432ff50ebeecf70cf7701928a2a59
Artem <artemlask2014@gmail.com>
2022-10-31 12:45:23 +0200

    add task 1 from Apiko

--- script.js
+++ script.js
@@ -9,3 +9,15 @@ function calculateSumOfArray() {
    console.log(sumOfNumbers);
 }
    calculateSumOfArray();
+
+   function printPowsOf2(numberOfTask) {
+        if(typeof (numberOfTask) === 'number') {
+            for (let i = 1; i < numberOfTask; i = i*2){
+                console.log(i);
+            }
+    }
+}
+   printPowsOf2("302");
+   printPowsOf2(null);
+   printPowsOf2(128);
+   printPowsOf2(60);
